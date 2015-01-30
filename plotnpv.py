a1338de22f6bc84adbc4deb830856263fe0d8fed
Francesco <francesco@dhcp-229-233.slac.stanford.edu>
2015-01-30 15:54:01 -0800

    added jet mass and width

--- plotnpv.py
+++ plotnpv.py
@@ -2,17 +2,22 @@ from numpy import load,mean,std,array,median
 
 pltdir = '../plots/'
 
-def getmeans(jet='j0'):
-    resdict = load('../output/resvsnpv_'+jet+'.npy')
-    keys = [10,20,30,40,50]
+ptbin = 'pt2030'
+
+jettypes = ['jnoarea0','jnoarea5','j0','j5']
+labels = ['inclusive','cluster $JVF>0.5$','area correction','cluster $JVF>0.5$ + area correction']
+
+keys = [35,45,55,65]
+
+def getmeans(jet='j0',var='res'):
+    resdict = load('../output/resvsnpv_'+jet+'_'+ptbin+'.npy')
     resdict = resdict[()]
     y = [mean(resdict[k]) for k in keys]
     x = [k-5 for k in keys]
     return x,y
 
-def getstds(jet='j0'):
-    resdict = load('../output/resvsnpv_'+jet+'.npy')
-    keys = [10,20,30,40,50]
+def getstds(jet='j0',var='res'):
+    resdict = load('../output/resvsnpv_'+jet+'_'+ptbin+'.npy')
     resdict = resdict[()]
     y = [std(resdict[k]) for k in keys]
     x = [k-5 for k in keys]
@@ -23,48 +28,37 @@ from matplotlib import rc
 rc('text', usetex=True)
 
 
-x,y = getmeans('j0')
-j0 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getmeans('j1')
-j1 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getmeans('j2')
-j2 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getmeans('j3')
-j3 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getmeans('j4')
-j4 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getmeans('j5')
-j5 = plt.plot(x,y,marker='o',linestyle='--')
+def plotvar(var):
 
-plt.ylim([-10,0])
-plt.xlim([0,50])
-plt.xlabel(r'NPV')
-plt.ylabel(r'$<p_T^{reco}-p_T^{true}>$ [GeV]')
-#plt.legend([j0res,j1res,j2res,j3res,j4res,j5res],
-#           ['incl.','$corrJVF>0.1$','$corrJVF>0.2$','$corrJVF>0.3$','$corrJVF>0.4$','$corrJVF>0.5$'],
-#           loc='upper left',frameon=False,numpoints=1)
-plt.savefig(pltdir+'jresvsnpvmean.png')
-plt.close()
+    varlabel = {'res':(r'$<p_T^{reco}-p_T^{true}>$ [GeV]',r'$\sigma(p_T^{reco}-p_T^{true})$ [GeV]'),
+                'mass':(r'$<jet~fmass>$ [GeV]',r'$\sigma(jet~mass)$ [GeV]'),
+                'width':(r'$<jet~width>$',r'$\sigma(jet~width)$')
+                }
 
-x,y = getstds('j0')
-j0 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getstds('j1')
-j1 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getstds('j2')
-j2 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getstds('j3')
-j3 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getstds('j4')
-j4 = plt.plot(x,y,marker='o',linestyle='--')
-x,y = getstds('j5')
-j5 = plt.plot(x,y,marker='o',linestyle='--')
+    for jt,l in zip(jettypes,labels):
+        x,y = getmeans(jt,var)
+        jetplot = plt.plot(x,y,marker='o',linestyle='--',label=l)
 
-plt.ylim([0,10])
-plt.xlim([0,50])
-plt.xlabel(r'NPV')
-plt.ylabel(r'$\sigma(p_T^{reco}-p_T^{true})$ [GeV]')
-#plt.legend([j0res,j1res,j2res,j3res,j4res,j5res],
-#           ['incl.','$corrJVF>0.1$','$corrJVF>0.2$','$corrJVF>0.3$','$corrJVF>0.4$','$corrJVF>0.5$'],
-#           loc='upper left',frameon=False,numpoints=1)
-plt.savefig(pltdir+'jresvsnpvstd.png')
-plt.close()
+#plt.ylim([-10,0])
+    plt.xlim([25,65])
+    plt.xlabel(r'NPV')
+    plt.ylabel(varlabel[var][0])
+    plt.legend(loc='upper left',frameon=False,numpoints=1)
+    plt.savefig(pltdir+'j'+var+'vsnpvmean'+'_'+ptbin+'.png')
+    plt.close()
+
+    for jt,l in zip(jettypes,labels):
+        x,y = getstds(jt)
+        jetplot = plt.plot(x,y,marker='o',linestyle='--',label=l)
+
+#plt.ylim([0,10])
+    plt.xlim([25,65])
+    plt.xlabel(r'NPV')
+    plt.ylabel(varlabel[var][1])
+    plt.legend(loc='upper left',frameon=False,numpoints=1)
+    plt.savefig(pltdir+'j'+var+'vsnpvstd_'+ptbin+'.png')
+    plt.close()
+
+
+for var in ['res','mass','width']:
+    plotvar(var)
--- storenpv.py
+++ storenpv.py
@@ -1,36 +1,56 @@
 import ROOT as r
 from sys import stdout
-filename = '../data/PythJXmc12aJETMETshort.root'
+filename = '../data/mc12_14TeV_Pythia8_J2_ITK_140_140.root'
 
 ff = r.TFile(filename)
 tree = ff.Get('tree0/tree')
 nentries = tree.GetEntries()
+#nentries = 1000
 
-cuts = [10,20,30,40,50]
-resdict = dict(zip(cuts,[[] for c in cuts]))
+cuts = [35,45,55,65]
+resdict = {c:[] for c in cuts}
+massdict = {c:[] for c in cuts}
+widthdict = {c:[] for c in cuts}
 
-jet = 'j4'
+ptmin = 20
+ptmax = 30
+ptbin = 'pt%d%d'%(ptmin,ptmax)
 
-nentries = tree.GetEntries()
+jet = 'j0'
+
 for jentry in xrange(nentries):
     tree.GetEntry(jentry)
     
-    if not jentry%100:
+    if not jentry%1000:
         stdout.write('\r%d'%jentry)
         stdout.flush()
 
     jpts = getattr(tree,'%spt'%jet)
     tjpts = getattr(tree,'t%spt'%jet)
-    resjets = [jpt-tjpt for jpt,tjpt in zip(jpts,tjpts)
-            if tjpt>20 and tjpt<30]
+    jms = getattr(tree,'%smass'%jet)
+    jws = getattr(tree,'%swidth'%jet)
 
-    npv = tree.NPVtruth
+    resjets = []
+    massjets = []
+    widthjets = []
+    for jpt,tjpt,jm,jw in zip(jpts,tjpts,jms,jws):
+        if tjpt<ptmin or tjpt>ptmax: continue
+        resjets.append(jpt-tjpt)
+        massjets.append(jm)
+        widthjets.append(jw)
+            
+
+    npv = tree.NPV
 
     for cut in cuts:
         if npv<cut:
             resdict[cut] += resjets
+            massdict[cut] += massjets
+            widthdict[cut] += massjets
             break
 
 
 from numpy import save
-save('../output/resvsnpv_'+jet,resdict)
+save('../output/resvsnpv_'+jet+'_'+ptbin,resdict)
+save('../output/massvsnpv_'+jet+'_'+ptbin,massdict)
+save('../output/widthvsnpv_'+jet+'_'+ptbin,widthdict)
