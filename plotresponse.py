8ba424dd900ad08a44b35b86ffb746dca38495f2
Francesco <francesco@vpn-g2-133-34.slac.stanford.edu>
2015-02-11 14:45:03 -0800

    add closure test

--- plotresponse.py
+++ plotresponse.py
@@ -63,3 +63,44 @@ print resparams
 import json
 with open('../output/jetresponse_'+pu+'.json','w') as outfile:
     json.dump(resparams,outfile)
+
+#closure
+
+from jetutils import Calibration
+calibdict = {key: Calibration(key,pu) for key in jets}
+
+from numpy import vectorize
+
+for jet in jets:
+    truepts = load('../output/truepts_'+jet+'_'+pu+'.npy')
+    recopts = load('../output/recopts_'+jet+'_'+pu+'.npy')
+    vgetpt = vectorize(calibdict[jet].getpt)
+    recopts = vgetpt(recopts)
+    responses = recopts/truepts
+    ptbins = digitize(truepts,ptedges)
+    avgres = []
+    avgpt = []
+    for ptbin in xrange(1,len(ptedges)): 
+        resdata = responses[ptbins==ptbin]
+        ptdata = recopts[ptbins==ptbin]
+        if ptbin==1: 
+            resdata = resdata[resdata<mean(resdata)+1.5*std(resdata)]
+            ptdata = ptdata[resdata<mean(resdata)+1.5*std(resdata)]
+        gfunc = norm
+        (mu,sigma) = gfunc.fit(resdata)
+        n,bins,patches = plt.hist(resdata,normed=True,bins=50)
+        y = gfunc.pdf( bins, mu, sigma)
+        l = plt.plot(bins, y, 'r--', linewidth=2)
+        plt.xlabel('$p_T^{reco}/p_T^{true}$')
+        plt.ylabel('a.u.')
+        plt.savefig('../plots/closure_resbin%d_'%ptbin+jet+'_'+pu+'.png')
+        plt.close()
+        avgres.append(mu)
+        avgpt.append(mean(ptdata))
+
+    xp = linspace(20,200,100)
+    plt.plot(recopts,responses,'.',avgpt,avgres,'o')
+    plt.xlabel('$p_T^{reco}$ [GeV]')
+    plt.ylabel('$p_T^{reco}/p_T^{true}$')
+    plt.savefig('../plots/closure_jetresponse_'+jet+'_'+pu+'.png')
+    plt.close()
