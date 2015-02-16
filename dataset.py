fdd5472439c963c719afde53e18c9fd180e36adf
Francesco <francesco@ATLAS-PC92145.local>
2015-02-16 09:52:52 -0800

    use new file

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@
 def getsamp(mu='mu20'):
     sampdict = {
         'mu140':'mc12_14TeV_Pythia8_J2_ITK_140_140.root',
-        'mu20':'PythiaJ2mc12aJETMET.root',
-        'mu20_clpt2':'PythiaJ2mc12aJETMET_clpt2.root',
+        'mu20':'PythJ1and2mc12aJETMET_short.root',
+#        'mu20_clpt2':'PythiaJ2mc12aJETMET_clpt2.root',
         }
     return sampdict[mu]
