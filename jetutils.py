b48d7b2c5d729c861b959d072329a948be8c8eb3
Francesco <francesco@dhcp-229-233.slac.stanford.edu>
2015-02-17 07:44:40 -0800

    add npv correction

--- jetutils.py
+++ jetutils.py
@@ -4,12 +4,22 @@ from numpy import log
 class Calibration(object):
     
     def __init__(self,jettype='j0',pu='mu20'):
-        jetresdict = json.load(open('../output/jetresponse_mu20.json'))
+        jetresdict = json.load(open('../output/jetresponse_'+pu+'.json'))
         self.a = jetresdict[jettype][0]
         self.b = jetresdict[jettype][1]
 
     def getpt(self,jetpt):
         return float(jetpt)/(self.b + self.a/log(jetpt))
+
+class NPVCorrection(object):
+    
+    def __init__(self,jettype='j0',pu='mu20'):
+        jetresdict = json.load(open('../output/npvcorrection_'+pu+'.json'))
+        self.a = jetresdict[jettype][0]
+        self.b = jetresdict[jettype][1]
+
+    def getpt(self,jetpt,npv):
+        return jetpt-self.b*npv-self.a
     
 
 #test = Calibration()
