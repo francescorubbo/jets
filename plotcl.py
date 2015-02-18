2089d6ba20e2b2b4b6b1d0a0a4cf9d486434795b
Francesco <francesco@dhcp-229-233.slac.stanford.edu>
2015-02-18 11:24:09 -0800

    plot w/o pt cut

--- plotcl.py
+++ plotcl.py
@@ -12,15 +12,22 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
 
-hshighpt = cljvf[(matchedcl) & (clpt>2.)]
-hslowpt = cljvf[(matchedcl) & (clpt<2.)]
-puhighpt = cljvf[(matchedcl==False) & (clpt>2.)]
-pulowpt = cljvf[(matchedcl==False) & (clpt<2.)]
-plt.hist(hshighpt,normed=True,label='matched to HS, $p_T>2$ GeV',alpha=0.5)
-plt.hist(hslowpt,normed=True,label='matched to HS, $p_T<2$ GeV',alpha=0.5)
-plt.hist(puhighpt,normed=True,label='unmatched to HS, $p_T>2$ GeV',alpha=0.5,histtype='step')
-plt.hist(pulowpt,normed=True,label='unmatched to HS, $p_T<2$ GeV',alpha=0.5,histtype='step')
+#hshighpt = cljvf[(matchedcl) & (clpt>2.)]
+#hslowpt = cljvf[(matchedcl) & (clpt<2.)]
+hslowpt = cljvf[(matchedcl)]
+#puhighpt = cljvf[(matchedcl==False) & (clpt>2.)]
+#pulowpt = cljvf[(matchedcl==False) & (clpt<2.)]
+pulowpt = cljvf[(matchedcl==False)]
+
+#plt.hist(hshighpt,normed=True,label='matched to HS, $p_T>2$ GeV',alpha=0.5)
+#plt.hist(hslowpt,normed=True,label='matched to HS, $p_T<2$ GeV',alpha=0.5)
+#plt.hist(puhighpt,normed=True,label='unmatched to HS, $p_T>2$ GeV',alpha=0.5,histtype='step')
+#plt.hist(pulowpt,normed=True,label='unmatched to HS, $p_T<2$ GeV',alpha=0.5,histtype='step')
+plt.hist(hslowpt,normed=True,label='matched to HS jets',alpha=0.5,histtype='step',color='r')
+plt.hist(pulowpt,normed=True,label='non-matched to HS jets',alpha=0.5,histtype='step',color='b')
+
 plt.ylim([0.001,100])
 plt.yscale('log')
+plt.xlabel('corrCVF')
 plt.legend()
-plt.savefig('testlog.png')
+plt.savefig(pltdir+'cvf.png')
