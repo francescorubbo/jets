bb315894c3c7ec46bcc31f514450b646584e23c7
Francesco <francesco@dhcp-229-233.slac.stanford.edu>
2015-02-20 14:26:18 -0800

    add errors

--- plotnpv.py
+++ plotnpv.py
@@ -1,4 +1,4 @@
-from numpy import load,mean,std,array,median
+from numpy import load,mean,std,array,median,sqrt
 import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
@@ -17,7 +17,7 @@ labels = ['inclusive','$CVF>0.5$','area correction','$CVF>0.5$ + area correction
           r'Voronoi ($p_T>\rho\cdot A$)']
 
 #keys = [35,45,55,65]
-keys = [20,25,30,35,40]
+keys = [20,25,30,35,40,45]
 
 import json
 
@@ -47,8 +47,9 @@ def getstds(jet='j0',var='res'):
     resdict = json.load(open('../output/'+var+'vsnpv_'+jet+'_'+ptbin+'_'+mu+'.json'))
     y = [std(resdict['%d'%k]) for k in keys]
 #    y = [fitgaus(resdict['%d'%k],jet) for k in keys]
+    yerr = array(y)/sqrt(2*len(resdict['%d'%k])-1)
     x = [k-2.5 for k in keys]
-    return x,y
+    return x,y,yerr
     
 def plotvar(var):
 
@@ -71,9 +72,9 @@ def plotvar(var):
     plt.close()
 
     for jt,l in zip(jettypes,labels):
-        x,y = getstds(jt,var)
+        x,y,yerr = getstds(jt,var)
         plt.figure(0)
-        jetplot = plt.plot(x,y,marker='o',linestyle='--',label=l)
+        jetplot = plt.errorbar(x,y,yerr=yerr,marker='o',linestyle='--',label=l)
 
 #plt.ylim([0,10])
     plt.xlim([min(keys)-5,max(keys)+5])
