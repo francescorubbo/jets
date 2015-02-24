efa487b45b8795b1285b75ac72c9216c0e7d8775
Francesco <francesco@vpn-g1-132-71.slac.stanford.edu>
2015-02-24 09:25:45 -0800

    b2bjvt development

--- plotROC.py
+++ plotROC.py
@@ -1,67 +1,38 @@
-import ROOT as r
-from sys import stdout
-
-mu = 'mu20'
-from dataset import getsamp
-filename = '../data/'+getsamp(mu)
-
-ff = r.TFile(filename)
-tree = ff.Get('tree1/tree')
-nentries = tree.GetEntries()
-
-ptmin = 60
-ptmax = 100
-ptbin = 'pt%d%d'%(ptmin,ptmax)
-
-nhs = 0
-npu = 0
-from numpy import arange,concatenate
-cuts = concatenate([arange(0,1,0.1),arange(0.91,1,0.01)])
-cutkeys = ['%1.2f'%cut for cut in cuts]
-nhsjvt = dict(zip(cutkeys,[0]*len(cuts)))
-npujvt = dict(zip(cutkeys,[0]*len(cuts)))
-
-for jentry in xrange(nentries):
-    
-    tree.GetEntry(jentry)
-
-    if not jentry%10:
-        stdout.write('\r%d'%jentry)
-        stdout.flush()
-
-    for jpt,jeta,tjpt,jvt in zip(tree.jpt,tree.jeta,tree.tjpt,tree.jb2bjvt):
-        if jeta<2.5: continue
-        if jpt<ptmin: continue
-        if jpt>ptmax: continue
-
-        if tjpt>0.:
-            nhs += 1
-            for cut in cuts:
-                if jvt>cut or jvt<0:
-                    nhsjvt['%1.2f'%cut] += 1
-        else:
-            npu += 1
-            for cut in cuts:
-                if jvt>cut or jvt<0:
-                    npujvt['%1.2f'%cut] += 1
-        
-
-from math import sqrt
-def efferr(k,N):
-    return sqrt(k*(1-float(k)/N))/N
-
-efficiency = [float(nhsjvt[cut])/nhs for cut in cutkeys]
-efficiencyerr = [efferr(nhsjvt[cut],nhs) for cut in cutkeys]
-mistag = [float(npujvt[cut])/npu for cut in cutkeys]
-mistagerr = [efferr(npujvt[cut],npu) for cut in cutkeys]
-
 import matplotlib.pyplot as plt
+plt.style.use('atlas')
 from matplotlib import rc
 rc('text', usetex=True)
 
-plt.errorbar(efficiency,mistag,xerr=efficiencyerr,yerr=mistagerr,fmt='o--')
+ptbin = 'pt2030'
+
+vars = ['jb2bjvt','jb2bcorrjvf','jb2brpt','jb2bjvt_trk','jb2bcorrjvf_trk','jb2brpt_trk']
+
+labeldict = {
+    'jb2bjvt':'B2BJVT',
+    'jb2bcorrjvf':'B2BcorrJVF',
+    'jb2brpt':'B2BRpT',
+    'jb2bjvt_trk':'track B2BJVT',
+    'jb2bcorrjvf_trk':'track B2BcorrJVF',
+    'jb2brpt_trk':'track B2BRpT'
+    }
+
+import json
+def plot(var='jb2bjvt'):
+    rocdict = json.load(open('../output/roc_'+var+'_'+ptbin+'.json'))
+    efficiency = rocdict['efficiency']
+    efficiencyerr = rocdict['efficiencyerr']
+    mistag = rocdict['mistag']
+    mistagerr = rocdict['mistagerr']
+
+    plt.errorbar(efficiency,mistag,
+                 xerr=efficiencyerr,yerr=mistagerr,
+                 fmt='o--',
+                 label=labeldict[var])
+
+for var in vars:
+    plot(var)
 plt.xlabel('Efficiency')
 plt.ylabel('Fake Rate')
-
-plt.savefig('../plots/b2bjvt_'+ptbin+'.png')
+plt.legend(loc='upper left')
+plt.savefig('../plots/roc_'+ptbin+'.png')
 
---
+++ storeROC.py
@@ -0,0 +1,68 @@
+import ROOT as r
+from sys import stdout,argv
+
+mu = 'mu20_b2bjvt'
+from dataset import getsamp
+filename = '../data/'+getsamp(mu)
+
+var = argv[1]
+
+ff = r.TFile(filename)
+tree = ff.Get('tree1/tree')
+nentries = tree.GetEntries()
+
+ptmin = 20
+ptmax = 30
+ptbin = 'pt%d%d'%(ptmin,ptmax)
+
+nhs = 0
+npu = 0
+from numpy import arange,concatenate
+cuts = concatenate([arange(0,1,0.1),arange(0.91,1,0.01)])
+cutkeys = ['%1.2f'%cut for cut in cuts]
+nhsjvt = dict(zip(cutkeys,[0]*len(cuts)))
+npujvt = dict(zip(cutkeys,[0]*len(cuts)))
+
+for jentry in xrange(nentries):
+    
+    tree.GetEntry(jentry)
+
+    if not jentry%1000:
+        stdout.write('\r%d'%jentry)
+        stdout.flush()
+
+    for jpt,jeta,tjpt,jvt in zip(tree.jpt,tree.jeta,tree.tjpt,getattr(tree,var)):
+        if jeta<2.5: continue
+        if jpt<ptmin: continue
+        if jpt>ptmax: continue
+
+        if tjpt>10.:
+            nhs += 1
+            for cut in cuts:
+                if jvt>cut or jvt<0:
+                    nhsjvt['%1.2f'%cut] += 1
+        else:
+            npu += 1
+            for cut in cuts:
+                if jvt>cut or jvt<0:
+                    npujvt['%1.2f'%cut] += 1
+        
+
+from math import sqrt
+def efferr(k,N):
+    return sqrt(k*(1-float(k)/N))/N
+
+efficiency = [float(nhsjvt[cut])/nhs for cut in cutkeys]
+efficiencyerr = [efferr(nhsjvt[cut],nhs) for cut in cutkeys]
+mistag = [float(npujvt[cut])/npu for cut in cutkeys]
+mistagerr = [efferr(npujvt[cut],npu) for cut in cutkeys]
+
+rocdict = {'efficiency': efficiency,
+           'efficiencyerr': efficiencyerr,
+           'mistag': mistag,
+           'mistagerr': mistagerr,
+           }
+
+import json
+with open('../output/roc_'+var+'_'+ptbin+'.json','w') as outfile:
+    json.dump(rocdict,outfile)
