245c884b4cf5064cc9aac554153b5c4d9ca446a8
francescorubbo <rubbo.francesco@gmail.com>
2015-03-06 13:57:15 -0800

    update

--- dataset.py
+++ dataset.py
@@ -3,8 +3,9 @@ def getsamp(mu='mu20'):
     sampdict = {
         'mu140':'mc12_14TeV_Pythia8_J2_ITK_140_140.root',
         'mu20':'PythJ1and2mc12aJETMET_short.root',
-        'mu20_b2bjvt':'PythJ1and2mc12aJETMET_short_b2bjvt.root',
-        'mu20_b2bjvt_trkjet':'PythJ1and2mc12aJETMET_short_b2bjvt_trkjet.root'
+        'mu20_b2bjvt_jetpt20_trkjetpt5':'PythJ1and2mc12aJETMET_short_b2bjvt_jetpt20_trkjetpt5.root',
+        'vbf_b2bjvt_jetpt20_trkjetpt5':'VBFH125_ZZ4lep_jetpt20_trkjetpt5.root',
+        'vbf_b2bjvt_jetpt10_trkjetpt2':'VBFH125_ZZ4lep_jetpt10_trkjetpt2.root'
 #        'mu20_clpt2':'PythiaJ2mc12aJETMET_clpt2.root',
         }
     return sampdict[mu]
