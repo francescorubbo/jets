abddceea1810ade6a8a2ed75e06f7f1ff87774f9
francescorubbo <rubbo.francesco@gmail.com>
2015-03-10 09:56:11 -0700

    add scan matching efficiencies

--- dataset.py
+++ dataset.py
@@ -5,7 +5,8 @@ def getsamp(mu='mu20'):
         'mu20':'PythJ1and2mc12aJETMET_short.root',
         'mu20_b2bjvt_jetpt20_trkjetpt5':'PythJ1and2mc12aJETMET_short_b2bjvt_jetpt20_trkjetpt5.root',
         'vbf_b2bjvt_jetpt20_trkjetpt5':'VBFH125_ZZ4lep_jetpt20_trkjetpt5.root',
-        'vbf_b2bjvt_jetpt10_trkjetpt2':'VBFH125_ZZ4lep_jetpt10_trkjetpt2.root'
+        'vbf_b2bjvt_jetpt10_trkjetpt2':'VBFH125_ZZ4lep_jetpt10_trkjetpt2.root',
+        'vbf_b2bjvt_scan':'VBFH125_ZZ4lep_scanmatchingeff.root'
 #        'mu20_clpt2':'PythiaJ2mc12aJETMET_clpt2.root',
         }
     return sampdict[mu]
