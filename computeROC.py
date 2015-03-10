3a62b80450bc522a6cd933a0f6fbd1c268e2dbf9
francescorubbo <rubbo.francesco@gmail.com>
2015-03-10 09:55:21 -0700

    update

--- computeROC.py
+++ computeROC.py
@@ -7,8 +7,8 @@ from matplotlib import rc
 rc('text', usetex=True)
 plt.style.use('atlas')
 
-ptmin = 20
-ptmax = 30
+ptmin = 30
+ptmax = 40
 
 fwdptallhs = array(json.load(open('../output/fwdall_jishs.json')))
 fwdptallpu = array(json.load(open('../output/fwdall_jispu.json')))
@@ -49,20 +49,31 @@ def geteffs(cut):
     nfake = allpu-matchedpu+taggedpu
     fake = float(nfake)/allpu
     fakeerror = efferr(nfake,allpu)
-    if fake<0.55: print cut,eff,fake
+    if eff>0.87 and fake<0.7:
+        print cut,eff,fake
     return eff,efferror,fake,fakeerror
 
 
 ptcuts = [10,15,20,25,30]
+colors = ['b','g','r','c','m']
 dphicuts = [0.2,0.4,0.6,0.8,1.5]
 asymmcuts = [0.1,0.2,0.3,0.4,1.0]
+markers = ['o','v','^','*','D']
 
-for ptcut in  ptcuts:
-    cuts = ['pt%d_dphi%1.1f_a%1.1f'%(ptcut,dphi,a) for dphi in dphicuts for a in asymmcuts]
-    (effs,efferrors,fakes,fakeerrors) = zip(*[geteffs(cut) for cut in cuts])
-    plt.errorbar(effs,fakes,xerr=efferrors,yerr=fakeerrors,fmt='o',label='$p_T^{ctl}>%d$ GeV'%ptcut)
+for ptcut,col in zip(ptcuts,colors):
+    for a,marker in zip(asymmcuts,markers):
+        cuts = ['pt%d_dphi%1.1f_a%1.1f'%(ptcut,dphi,a) for dphi in dphicuts]
+        (effs,efferrors,fakes,fakeerrors) = zip(*[geteffs(cut) for cut in cuts])
+#        plt.errorbar(effs,fakes,xerr=efferrors,yerr=fakeerrors,fmt=marker,color=col,
+#                     label='$p_T^{ctl}>%d$ $GeV$, $A_{p_T}<%1.1f$'%(ptcut,a))
+        plt.plot(effs,fakes,marker,color=col,
+                 label='$p_T^{ctl}>%d$ $GeV$, $A_{p_T}<%1.1f$'%(ptcut,a))
 
-plt.xlabel('Efficiency')    
-plt.ylabel('Fake rate')    
-plt.legend(loc='upper left')
-plt.savefig('test.png')
+#plt.plot([0,1],[0,1],'k--')
+plt.xlabel('Efficiency')
+plt.ylabel('Fake rate')
+#plt.ylim([0.4,1.0])
+#plt.xlim([0.6,1.0])
+lgd = plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=5, borderaxespad=0.,fontsize=6)
+plt.savefig('test.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
