dc0d22dc81b735634a27b36e619b6a2294c2ff87
francescorubbo <rubbo.francesco@gmail.com>
2015-03-10 09:56:47 -0700

    add reference line

--- plotROC.py
+++ plotROC.py
@@ -6,12 +6,17 @@ rc('text', usetex=True)
 ptbin = 'pt3040'
 
 vars = ['jb2bcorrjvf','jb2bcorrjvf_trk2','jb2bcorrjvf_trk4']
+vars = ['jb2bjvt','jb2bjvt_70','jb2bjvt_75','jb2bjvt_85','jb2bjvt_90']
 
 labeldict = {
     'jjvt':'JVT',
     'jcorrjvf':'corrJVF',
     'jrpt':'$R_{p_T}$',
     'jb2bjvt':'B2BJVT',
+    'jb2bjvt_70':'B2BJVT, 70\%',
+    'jb2bjvt_75':'B2BJVT, 75\%',
+    'jb2bjvt_85':'B2BJVT, 85\%',
+    'jb2bjvt_90':'B2BJVT, 90\%',
     'jb2bcorrjvf':'B2BcorrJVF',
     'jb2brpt':'B2BRpT',
     'jb2bjvt_trk':'track B2BJVT',
@@ -36,11 +41,11 @@ def plot(var='jb2bjvt'):
 
 for var in vars:
     plot(var)
+#plt.plot([0,1],[0,1],'k--')
 plt.xlabel('Efficiency')
 plt.ylabel('Fake Rate')
 plt.legend(loc='upper left')
-plt.xlim([0.75,1.0])
-#plt.yscale('log')
-plt.ylim([0.4,1.0])
+#plt.ylim([0.6,0.95])
+#plt.xlim([0.7,1.0])
 plt.savefig('../plots/roc_'+ptbin+'.png')
 
