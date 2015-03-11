109964197050ad7bd2ffb6d4a1bec493cf2cb4f5
Aviv Cukierman <avivcukierman@dn0a22c616.sunet>
2015-03-11 15:39:54 -0700

    Added sigma rho study dataset

--- dataset.py
+++ dataset.py
@@ -3,6 +3,6 @@ def getsamp(mu='mu20'):
     sampdict = {
         'mu140':'mc12_14TeV_Pythia8_J2_ITK_140_140.root',
         'mu20':'PythJ1and2mc12aJETMET_short.root',
-#        'mu20_clpt2':'PythiaJ2mc12aJETMET_clpt2.root',
+        'sigma_rho_study':'20150309.17.35_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
         }
     return sampdict[mu]
