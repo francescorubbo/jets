200a7e19adc307bcc5a1ddff7a452502d54dd4dd
Aviv Cukierman <avivcukierman@Avivs-MacBook-Pro.local>
2015-03-23 23:21:56 -0700

    Changed fit function to quadratic in log

--- jetutils.py
+++ jetutils.py
@@ -7,9 +7,10 @@ class Calibration(object):
         jetresdict = json.load(open('../output/jetresponse_'+pu+'.json'))
         self.a = jetresdict[jettype][0]
         self.b = jetresdict[jettype][1]
+        self.c = jetresdict[jettype][2]
 
     def getpt(self,jetpt):
-        return float(jetpt)/(self.b + self.a/log(jetpt))
+        return float(jetpt)/(self.a + self.b*log(jetpt) + self.c*log(jetpt)**2)
 
 class NPVCorrection(object):
     
