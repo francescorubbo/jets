5eba2110d973f39784a978c350990fa975338fc0
Aviv Cukierman <acukierm@atlint03.slac.stanford.edu>
2015-04-01 16:43:43 -0700

    Included dataset with voronoi+cvf algos

--- dataset.py
+++ dataset.py
@@ -4,5 +4,6 @@ def getsamp(mu='mu20'):
         'mu140':'mc12_14TeV_Pythia8_J2_ITK_140_140.root',
         'mu20':'PythJ1and2mc12aJETMET_short.root',
         'sigma_rho_study':'20150309.17.35_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
+        'voronoi_cvf':'20150330.09.50_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
         }
     return sampdict[mu]
