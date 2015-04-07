5faeb4d47e8836987dc79d7f9f13e4e4feea3f6d
Aviv Cukierman <acukierm@atlint03.slac.stanford.edu>
2015-04-06 17:36:11 -0700

    Added dataset with minDR

--- dataset.py
+++ dataset.py
@@ -4,6 +4,8 @@ def getsamp(mu='mu20'):
         'mu140':'mc12_14TeV_Pythia8_J2_ITK_140_140.root',
         'mu20':'PythJ1and2mc12aJETMET_short.root',
         'sigma_rho_study':'20150309.17.35_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
-        'voronoi_cvf':'20150330.09.50_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
+        #'voronoi_cvf':'20150330.09.50_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root',
+        'voronoi_cvf':'20150406.11.52_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root', #in /output/atlas/acukierm
+	'voronoi_cvf_short':'20150402.14.20_pileup.PythJ1and2mc12aJETMET_short.jetmet2012pileupcustom.root', #in /ProofAna/run
         }
     return sampdict[mu]
